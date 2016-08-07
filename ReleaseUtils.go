96ef6a2795bbb7ac9a5835bb9b53bd9ba5a476c3
brotherlogic <brotherlogic@gmail.com>
2016-08-06 18:51:07 -0700

    Adds slicer code to release utils. This closes #45.

--- ReleaseUtils.go
+++ ReleaseUtils.go
@@ -29,3 +29,33 @@ func sortByLabelCat(rel1 Release, rel2 Release) int {
 
 	return 0
 }
+
+// Split splits a releases list into buckets
+func Split(releases []*Release, n float64) [][]*Release{
+     var solution [][]*Release
+
+     var count int32
+     count = 0
+     for _, rel := range releases {
+     	 count += rel.FormatQuantity
+     }
+
+     boundaryAccumulator := float64(count) / n
+     boundaryValue := boundaryAccumulator
+     currentValue := 0.0
+     var currentReleases []*Release
+     for _, rel := range releases {
+     	 if currentValue + float64(rel.FormatQuantity) > boundaryValue {
+	    solution = append(solution, currentReleases)
+	    currentReleases = make([]*Release, 0)
+	    currentValue = 0.0
+	    boundaryValue += boundaryAccumulator
+	 }
+
+	 currentReleases = append(currentReleases, rel)
+	 currentValue += float64(rel.FormatQuantity)
+     }
+	    solution = append(solution, currentReleases)
+	    
+     return solution
+}
