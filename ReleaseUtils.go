97439637eb6d788ee64709076d652f79593e7433
Simon Tucker <brotherlogic@gmail.com>
2022-07-05 16:17:21 -0700

    Add redef to the master list

--- ReleaseUtils.go
+++ ReleaseUtils.go
@@ -81,7 +81,7 @@ func GetMainLabel(labels []*Label) *Label {
 
 		// Favour Mondiophone and Timing
 		for _, label := range labels {
-			for _, match := range []string{"Mondiophone", "Timing", "The Roundtable", "Schema"} {
+			for _, match := range []string{"Mondiophone", "Timing", "The Roundtable", "Schema", "Redefinition Records"} {
 				if match == label.GetName() {
 					return label
 				}
